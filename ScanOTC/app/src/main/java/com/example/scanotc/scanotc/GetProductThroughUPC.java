a779dc9ffdafb0522c9f92a9ea3f9646865b274c
Samuel Sanchez <sasanchez@csumb.edu>
2017-07-15 16:57:11 -0700

    Implemented Walmart Labs API search function within it's own class.

---
+++ ScanOTC/app/src/main/java/com/example/scanotc/scanotc/GetProductThroughUPC.java
@@ -0,0 +1,57 @@
+package com.example.scanotc.scanotc;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.Toast;
+
+/**
+ * Created by sham on 7/15/17.
+ */
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class GetProductThroughUPC extends AsyncTask<String, String, String> {
+    Context context;
+    AlertDialog alertDialog;
+    GetProductThroughUPC(Context ctx){
+        context = ctx;
+    }
+    @Override
+    protected String doInBackground(String... params){
+        // Walmart API URL Format: http://api.walmartlabs.com/v1/items?apiKey={apiKey}&upc=035000521019
+        // Key: w35vtdxspscd2sfhczjca3wt
+        String type = params[0];
+        String login_url = "http://api.walmartlabs.com/v1/items?apiKey=w35vtdxspscd2sfhczjca3wt&upc=035000521019";
+        try{
+            URL url = new URL(login_url);
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+            InputStream inputStream = httpURLConnection.getInputStream();
+            Log.i("Response", httpURLConnection.getResponseMessage());
+            BufferedReader bf = new BufferedReader(new InputStreamReader(inputStream));
+            String result = "";
+            String line = "";
+            while((line = bf.readLine()) != null){
+                result += line;
+            }
+            bf.close();
+            inputStream.close();
+            httpURLConnection.disconnect();
+            Log.i("Result", result);
+            return result;
+        }
+        catch(Exception e){
+            Log.i("Error", e.toString());
+        }
+
+        return "";
+    }
+}
--- ScanOTC/app/src/main/java/com/example/scanotc/scanotc/MainActivity.java
+++ ScanOTC/app/src/main/java/com/example/scanotc/scanotc/MainActivity.java
@@ -9,5 +9,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GetProductThroughUPC getProductThroughUPC = new GetProductThroughUPC(this);
+        String type = "";
+        String username = "";
+        String password = "";
+        getProductThroughUPC.execute(type, username, password);
     }
 }
