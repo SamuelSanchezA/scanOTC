6b94472a805f89140cd443e229e46a719fb19b04
Sandra Silverio <sandra.silverio@cloverhealth.com>
2017-07-16 02:55:46 -0700

    Minor ui changes.

--- ScanOTC/app/src/main/java/com/example/scanotc/scanotc/results.java
+++ ScanOTC/app/src/main/java/com/example/scanotc/scanotc/results.java
@@ -1,13 +1,34 @@
 package com.example.scanotc.scanotc;
 
 import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class results extends AppCompatActivity {
+
+    private ImageButton toCam;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
+
+        toCam = (ImageButton) findViewById(R.id.cam_button);
+
+        toCam.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                launchActivity();
+            }
+        });
+    }
+
+    private void launchActivity() {
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
